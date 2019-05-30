61395e1fe2aaa97dcb756bb6063e238dcb2fe9de
yango3 <o3.yang@hotmail.com>
2019-05-30 14:44:11 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -19,27 +19,6 @@ var CONTRACT_ADDR = '1GHzPAoYxzuT2aTwpwHx2z2rcaSo16pyUy';
 var CONTRACT_TABLE_NAME = 'IotDemo';
 var CONTRACT_KEY_NAME = 'state';
 
-// var postData = JSON.stringify({
-//     funName: 'view',
-//     args: {
-//         method: 'getMiners',
-//         params: {},
-//     }
-// })
-
-// var options = {
-//     hostname: '40.73.100.56',
-//     port: 18089,
-//     path: '/rpc',
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'Content-Length': postData.length
-//     }
-// };
-
-
-
 
 $.ready(function (error) {
     if (error) {
@@ -49,12 +28,6 @@ $.ready(function (error) {
     var stateSwitch = 0;
     setInterval(function () {
         console.log('\nDate:', new Date());
-
-        // client.call('getMiners', {}, function (resp, code) {
-        //     console.log('code:', code)
-        //     console.log('resp:')
-        //     console.log(resp);
-        // });
 
         client.call('getUserTableValue', {
             contractAddr: CONTRACT_ADDR,
@@ -68,42 +41,21 @@ $.ready(function (error) {
             handleWithState(resp);
 
         });
-        // console.log('\n control switch on/off');
 
-        // runList(function (err, data) {
-        //     if (err) {
-        //         console.log('error:', data);
-        //     } else {
-        //         console.log(data);
-        //     }
-        // })
-        // if (stateSwitch === 0) {
-        //     stateSwitch = 1;
-        //     console.log('\n control switch on');
-        //     runSwitch(1, function () {});
-        // } else {
-        //     stateSwitch = 0;
-        //     console.log('\n control switch off');
-        //     runSwitch(0, function () {});
-        // }
     }, 10000);
 });
 
 function handleWithState(resp) {
     var tempObj;
     console.log('\nhandleWithState:')
-    // console.log(resp);
-    // console.log(typeof resp)
+
     try {
         tempObj = JSON.parse(resp);
     } catch (e) {
         console.log('\nerr: ', e)
         return;
     }
-    // console.log('\ntempObj:');
-    // console.log(tempObj);
-    // console.log("err: ", tempObj.err);
-    // console.log('state:', tempObj.value)
+
 
     var stateSwitch = (tempObj.value + '').substr(1);
     console.log('switch: ', stateSwitch)
